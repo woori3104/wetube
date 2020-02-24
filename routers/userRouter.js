5be7b0321a9d2f5c4f73756223192826241c01cb
woori3104 <sizuku11261@gmail.com>
2020-02-24 20:51:55 +0900

    password 수정

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "../routes";
 import {
     getEditProfile,
-    changePassword,
+    getChangePassword,
+    postChangePassword,
     postEditProfile,
     getMe
 } from "../controller/userController";
@@ -12,7 +13,8 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar,postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate,changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 userRouter.get(routes.userDetail(), onlyPrivate, getMe);
 
 export default userRouter;
