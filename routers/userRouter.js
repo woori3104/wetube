0c7d9d677eff0a24d5af6cfd592c961bd2af7d5d
woori3104 <sizuku11261@gmail.com>
2020-02-24 20:22:36 +0900

    edit profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,12 +3,15 @@ import routes from "../routes";
 import {
     getEditProfile,
     changePassword,
+    postEditProfile,
     getMe
 } from "../controller/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar} from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar,postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate,changePassword);
 userRouter.get(routes.userDetail(), onlyPrivate, getMe);
 
