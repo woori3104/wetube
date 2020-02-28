8343e76619c1f2eff2574233d49ee479ea6df09e
woori3104 <sizuku11261@gmail.com>
2020-02-28 23:34:27 +0900

    조회수 연동

--- routes.js
+++ routes.js
@@ -35,6 +35,10 @@ const FACEBOOK_CALLBACK = "/auth/facebook/callback";
 const KAKAO = "/auth/kakao";
 const KAKAO_CALLBACK = "/auth/kakao/callback";
 
+//API
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
     home: HOME,
     join: JOIN,
@@ -80,7 +84,9 @@ const routes = {
     facebook: FACEBOOK,
     facebookCallback: FACEBOOK_CALLBACK,
     kakao: KAKAO,
-    kakaoCallback:KAKAO_CALLBACK
+    kakaoCallback: KAKAO_CALLBACK,
+    api: API,
+    registerView:REGISTER_VIEW
 
 };
 
